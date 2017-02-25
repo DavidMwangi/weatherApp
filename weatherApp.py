bade4304b0f91424266b47f1816a5ad41e9c8215
David Mwangi <David Mwangi>
2017-02-25 15:13:37 +0300

    Looped and added Escape. Escapes with an invalid city

--- weatherApp.py
+++ weatherApp.py
@@ -3,27 +3,37 @@ import re
 from urllib import request
 
 #http://www.weather-forecast.com/locations/Nakuru/forecasts/latest
-
-city = input("Enter city: ")
-
-url = "http://www.weather-forecast.com/locations/" + city + "/forecasts/latest"
-
-data = request.urlopen(url).read().decode('utf-8')
-
-#print(data)
-
-m = re.search('<span class="phrase">', data)
-
-start = m.end()
-
-data = data[start:]
-
-m = re.search("</span>", data)
-
-end = m.start()
-
-print(city.capitalize() + " Weather Forecast")
-
-print ("-" * (len(city)+17 ))
-
-print (data[0:end])
+while True:
+    
+    try:
+    
+        city = input("Enter city: ")
+        
+        url = "http://www.weather-forecast.com/locations/" + city + "/forecasts/latest"
+        
+        data = request.urlopen(url).read().decode('utf-8')
+        
+        #print(data)
+        
+        m = re.search('<span class="phrase">', data)
+        
+        start = m.end()
+        
+        data = data[start:]
+        
+        m = re.search("</span>", data)
+        
+        end = m.start()
+        
+        print(city.capitalize() + " Weather Forecast")
+        
+        print ("-" * (len(city)+17 ))
+        
+        print (data[0:end])
+        
+    except:
+        
+        print("Invalid City.")
+        
+        break
+    
